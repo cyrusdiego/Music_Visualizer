db8e019287a17fc15fd793a71e8d2f14fbdb1db4
daniel <drojasca>
2019-03-22 17:03:15 -0600

    finished fft needs testing

--- FFT.cpp
+++ FFT.cpp
@@ -3,13 +3,14 @@
 #include <cmath>
 #include <iostream>
 #include <vector>
+#include <complex<double>>
 double pi = 3.1415926535897; // use preprocessor or something
 using namespace std;
 
 // rearrange array
 // @param: array of audio signal (all real values)
-void rearrange(vector<pair<double,double>> &samples){
-    vector<pair<double,double>> even, odd;
+void rearrange(vector<complex<double>> &samples){
+    vector<complex<double>> even, odd;
     int N = samples.size();
     for(int i = 0; i < N; i++){
         if(i % 2 == 0){
@@ -33,7 +34,7 @@ void mag_phase(){
 
 // main fft function
 // double check type of the samples, how big of an int
-vector<pair<double,double>> FFT(vector<pair<double,double>> samples){
+vector<complex<double>> FFT(vector<complex<double>> samples){
     assert(log2(samples.size()) >= 0); // ensures that the size is 2^m m >= 0
 
     rearrange(samples);  // rearrange order for easier odd / even
@@ -43,30 +44,28 @@ vector<pair<double,double>> FFT(vector<pair<double,double>> samples){
         return samples;  // figure out what to actually return
     }
     else{
-        pair<double,double> W_N = make_pair(cos(2*pi/N),sin(2*pi/N));
-        double W = 1;
+        complex<double> W_N = polar(1,2*pi/N);
+        complex<double> W = (1,0);
 
         // splitting to even and odd
-        vector<pair<double,double>> sample_even;
-        vector<pair<double,double>> sample_odd;
+        vector<complex<double>> sample_even;
+        vector<complex<double>> sample_odd;
 
         samples_even.insert(samples_even.begin(),samples.begin(), samples.begin() + N/2);
         samples_even.insert(samples_even.begin(),samples.begin() + N/2 + 1, samples.end());
 
-        vector<pair<double,double>> Y_even = FFT(sample_even);
-        vector<pair<double,double>> Y_odd = FFT(sample_odd);
+        vector<complex<double>> Y_even = FFT(sample_even);
+        vector<complex<double>> Y_odd = FFT(sample_odd);
 
-        vector<pair<double,double>> Y;
+        vector<complex<double>> Y;
         // let c++ decide N/2 - 1
         for(int j = 0; j < N / 2 - 1; j++){
-
-
-
-
+            Y.at(j) = Y_even.at(j) + W*Y_odd.at(j);
+            Y.at(j + N/2) = Y_even.at(j) - W*Y_odd.at(j);
+            W *= W_N;
         }
-
+        return Y;
     }
 
-    return Y;
-    // return array of complex numbers in frequency domain
+    // return array of complex<double> numbers in frequency domain
 }
