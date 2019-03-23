494af680e5e65548e4a892828afe9965cca7266a
Cyrus <cdiego@ualberta.ca>
2019-03-23 12:22:38 -0600

    works, again

--- FFT.cpp
+++ FFT.cpp
@@ -7,12 +7,12 @@ const double pi = 3.141592653589793238460;
 using namespace std;
 
 typedef complex<double> complex_num;
-typedef vector<complex_num> samples;
+typedef vector<complex_num> complex_vec;
 
 // rearrange array
 // @param: array of audio signal (all real values)
-void rearrange(samples &samples){
-    samples even, odd;
+void rearrange(complex_vec &samples){
+    complex_vec even, odd;
     int N = samples.size();
     for(int i = 0; i < N; i++){
         if(i % 2 == 0){
@@ -32,7 +32,7 @@ void rearrange(samples &samples){
 
 // main fft function
 // double check type of the samples, how big of an int
-samples FFT(samples samples){
+complex_vec FFT(complex_vec samples){
     assert(log2(samples.size()) >= 0); // ensures that the size is 2^m m >= 0
 
     int N = samples.size();
@@ -42,23 +42,22 @@ samples FFT(samples samples){
     }
     else{
         rearrange(samples);  // rearrange order for easier odd / even
-        complex<double> W_N = polar(1.0,(-2*pi)/N);;  // root of Unity
-        complex<double> W(1,0);
+        complex_num W_N = polar(1.0,(-2*pi)/N);;  // root of Unity
+        complex_num W(1,0);
 
         // Divide
-        samples sample_even;
-        samples sample_odd;
+        complex_vec sample_even;
+        complex_vec sample_odd;
 
         sample_even.insert(sample_even.begin(),samples.begin(), samples.begin() + N/2);
         sample_odd.insert(sample_odd.begin(),samples.begin() + N/2, samples.end());
 
         // conquer
-        samples Y_even = FFT(sample_even);
-        samples Y_odd = FFT(sample_odd);
+        complex_vec Y_even = FFT(sample_even);
+        complex_vec Y_odd = FFT(sample_odd);
 
-        samples Y;
+        complex_vec Y;
         Y.assign(N,0);
-
 
         for(int j = 0; j < (N / 2); ++j){
             Y.at(j) = Y_even.at(j) + (W * Y_odd.at(j));
@@ -74,7 +73,7 @@ samples FFT(samples samples){
 int main(){
     double arr[8] = {0,1,2,3,4,5,6,7};
 
-    samples input;
+    complex_vec input;
     for(int i = 0; i < 8; i++){
         complex<double> temp(arr[i],0);
         input.push_back(temp);
@@ -83,7 +82,7 @@ int main(){
     for(auto i = input.begin(); i != input.end();i++){
         cout << real(*i) << " " << imag(*i) << endl;
     }
-    samples output = FFT(input);
+    complex_vec output = FFT(input);
     cout << "FFT results" << endl;
     for(auto i = output.begin(); i != output.end(); i++){
         cout << real(*i) << " " << imag(*i) << endl;
