f976a0bd6e8c46ef90c02902f36500c5cb911acb
Cyrus <cdiego@ualberta.ca>
2019-03-23 11:07:09 -0600

    testing...

--- FFT.cpp
+++ FFT.cpp
@@ -30,11 +30,8 @@ void rearrange(vector<complex<double>> &samples){
 
 // main fft function
 // double check type of the samples, how big of an int
-vector<complex<double>> FFT(vector<complex<double>> samples,string cmd){
-    cout << cmd << endl;
+vector<complex<double>> FFT(vector<complex<double>> samples,int N){
     assert(log2(samples.size()) >= 0); // ensures that the size is 2^m m >= 0
-    int N = samples.size(); // get size of array
-
 
     if(N == 1){
         return samples;  // figure out what to actually return
@@ -42,7 +39,7 @@ vector<complex<double>> FFT(vector<complex<double>> samples,string cmd){
     else{
         rearrange(samples);  // rearrange order for easier odd / even
 
-        complex<double> W_N = polar((double)1,2*pi/N);
+        complex<double> W_N = polar((double)1,-2*pi/N);
         complex<double> W(1,0);
 
         // splitting to even and odd
@@ -52,23 +49,13 @@ vector<complex<double>> FFT(vector<complex<double>> samples,string cmd){
         sample_even.insert(sample_even.begin(),samples.begin(), samples.begin() + N/2);
         sample_odd.insert(sample_odd.begin(),samples.begin() + N/2, samples.end());
 
-        vector<complex<double>> Y_even = FFT(sample_even,"even");
-        vector<complex<double>> Y_odd = FFT(sample_odd,"odd");
+        vector<complex<double>> Y_even = FFT(sample_even,sample_even.size());
+        vector<complex<double>> Y_odd = FFT(sample_odd,sample_even.size());
         vector<complex<double>> Y;
-        if(N == 4){
-            cout << Y_even.size() << endl;
-            cout << Y_odd.size() << endl;
-        }
-        int counter = 0;
+        Y.assign(N,0);
         for(int j = 0; j < N / 2; j++){
-            if(N == 2){
-                Y.insert(Y.begin() + j,Y_even.at(j) + W*Y_odd.at(j));
-                Y.insert(Y.begin() + j + N/2,Y_even.at(j) - W*Y_odd.at(j));
-            }
-            else{
-                Y.insert(Y.begin() + j,Y_even.at(j) + W*Y_odd.at(j));
-                Y.insert(Y.begin() + j + N/2 - 1,Y_even.at(j) - W*Y_odd.at(j));
-            }
+            Y.at(j) = Y_even.at(j) + (W * Y_odd.at(j));
+            Y.at(j + N/2) = Y_even.at(j) - (W * Y_odd.at(j));
 
             W *= W_N;
         }
@@ -80,13 +67,16 @@ vector<complex<double>> FFT(vector<complex<double>> samples,string cmd){
 }
 
 int main(){
-    int arr[] = {1,2,3,4};
+    int arr[4] = {1,2,3,4};
+    for(int i = 0; i < 4; i++){
+        arr[i] = i;
+    }
     vector<complex<double>> input;
     for(int i = 0; i < 4; i++){
         complex<double> temp((double)arr[i],0);
         input.push_back(temp);
     }
-    vector<complex<double>> output = FFT(input, "first");
+    vector<complex<double>> output = FFT(input, input.size());
     for(auto i = output.begin(); i != output.end(); i++){
         cout << real(*i) << " " << imag(*i) << endl;
     }
