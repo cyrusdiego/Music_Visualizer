6420fbf0519bf3856a392432c9ddb5684c5e0817
Cyrus <cdiego@ualberta.ca>
2019-04-06 11:51:43 -0600

    commented globals.h

--- globals.h
+++ globals.h
@@ -1,6 +1,6 @@
 /*
     NAMES: CYRUS DIEGO and DANIEL ROJAS-CARDONA
-    ID: 1528911 and
+    ID: 1528911 and 1531475
     CMPUT 275 WINTER 2019 Final Project: MUSIC VISUALIZER
 
     globals.h : header file containing content all files require
@@ -8,22 +8,29 @@
 
 #ifndef GLOBALS_H
 #define GLOBALS_H
+
+// SFML Modules
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <SFML/System.hpp>
+
+// STL / C++ Libraries 
 #include <string>
-#include <cassert>
+#include <cassert>  // for assert
 #include <cmath>  // for log2
 #include <math.h>
 #include <iostream>
-#include <unordered_map>
 #include <vector>
 #include <complex>
 #include <algorithm>
 #include <fstream>
 #include <map>
-const double pi = 3.141592653589793238460;
+
+// Constants
+const double pi = 3.141592653589793238460;  // needed for fft and hanning window
+
+// alias for complex vecs / numbers
 typedef std::complex <double> complex_num;
 typedef std::vector <complex_num> complex_vec;
 
